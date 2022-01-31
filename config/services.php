0aac57b8c549816597af55aa9e165445243404a3
Dale <xiangkeguo@gmail.com>
2022-01-31 21:58:01 +0800

    add resources

--- config/services.php
+++ config/services.php
@@ -29,5 +29,11 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'xcrm' => [
+        'endpoint' => env('XCRM_ENDPOINT'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
 
 ];
