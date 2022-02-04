52ac01b676ef4889d57834c6bc35ac044e909879
Dale <xiangkeguo@gmail.com>
2022-02-05 06:08:11 +0800

    认识你真好

--- app/Services/Resources/Zenai.php
+++ app/Services/Resources/Zenai.php
@@ -39,9 +39,9 @@ final class Zenai {
 
         if(in_array($keyword, array_keys($res))){
             $res = $res[$keyword];
-            $url = "http://cdnapi.yongbuzhixi.com/{$res['code']}{$date}.mp3";
+            $url = "http://xcrm.yongbuzhixi.com/api/redirect?target=http://dailyaudio-1253798207.file.myqcloud.com/{$res['code']}{$date}.mp3";
             if($keyword == '认识你真好')
-                $url = "https://silk.yongbuzhixi.com/vof/{$res['code']}{$date}.mp3";
+                $url = "http://xcrm.yongbuzhixi.com/api/redirect?target=https://febc-1253798207.file.myqcloud.com/vof/{$res['code']}{$date}.mp3";
             return [
             	'type' => 'music',
             	"data"=> [
