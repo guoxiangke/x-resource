270a612bf91cef6572e6a9649e16efcbad111b1c
Dale <xiangkeguo@gmail.com>
2022-02-04 16:02:41 +0800

    cdn of myqcloud

--- app/Services/Resources/Zenai.php
+++ app/Services/Resources/Zenai.php
@@ -17,23 +17,18 @@ final class Zenai {
             '喜乐灵程' =>[
                 'text' => "https://d7jf0n9s4n8dc.cloudfront.net/html/tljd/tljd{$date}.html",
                 'image' => 'https://img-1253798207.file.myqcloud.com/tljd.png',
-                'audio' => "http://dailyaudio-1253798207.file.myqcloud.com/tljd{$date}.mp3"
+                'code' => "tljd"
             ],
             '灵程真言' =>[
                 'text' => "https://d7jf0n9s4n8dc.cloudfront.net/html/tllczy/tllczy{$date}.html",
                 'image' => 'https://img-1253798207.file.myqcloud.com/tllczy.png',
-                'audio' => "http://dailyaudio-1253798207.file.myqcloud.com/tllczy{$date}.mp3"
+                'code' => "tllczy"
             ],
             '真爱驻我家' =>[
                 'text' => "https://d7jf0n9s4n8dc.cloudfront.net/html/tlsp/tlsp{$date}.html",
                 'image' => 'https://img-1253798207.file.myqcloud.com/tltl.png',
-                'audio' => "http://dailyaudio-1253798207.file.myqcloud.com/tlsp{$date}.mp3"
-            ],
-            '认识你真好' =>[
-                'text' => "https://d7jf0n9s4n8dc.cloudfront.net/html/vof/vof{$date}.html",
-                'image' => 'https://img-1253798207.file.myqcloud.com/vof.png',
-                'audio' => "https://febc-1253798207.file.myqcloud.com/vof/vof{$date}.mp3"
-            ],
+                'code' => "tlsp"
+            ]
         ];
 
         if(in_array($keyword, array_keys($res))){
@@ -41,7 +36,7 @@ final class Zenai {
             return [
             	'type' => 'music',
             	"data"=> [
-                    "url" => $res['audio'],
+                    "url" => "http://cdnapi.yongbuzhixi.com/{$res['code']}{$date}.mp3",
                     'title' => "【{$keyword}】{$date}",
                     'description' => '真爱驻我家',
                 ],
