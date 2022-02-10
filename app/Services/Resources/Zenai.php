22f4fe3937e02cf15f1177cfdb9ccb7e8f233545
Dale <xiangkeguo@gmail.com>
2022-02-10 08:19:11 +0800

    tlcp

--- app/Services/Resources/Zenai.php
+++ app/Services/Resources/Zenai.php
@@ -27,8 +27,8 @@ final class Zenai {
             ],
             '真爱驻我家' =>[ // 1 - 5 真爱驻我家 是周日周一休息
                 'imageCode' => 'tltl',
-                'code' => "tlsp",
-                'textCode' => "tlsp",
+                'code' => "tlpc",
+                'textCode' => "tlcp",
             ],
             '认识你真好' =>[
                 'imageCode' => 'vof',
