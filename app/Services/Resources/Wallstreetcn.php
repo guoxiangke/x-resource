c28595171d08c79228e6cf63455256187f2cf450
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2022-09-28 16:41:12 -0700

    wallstreetcn

--- app/Services/Resources/Wallstreetcn.php
+++ app/Services/Resources/Wallstreetcn.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use voku\helper\HtmlDomParser;
 
 final class Wallstreetcn{
 	public function __invoke($keyword) {
