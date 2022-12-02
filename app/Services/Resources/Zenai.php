ab7086397d57dd349c69ad749614b1531d26fb92
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2022-12-02 12:15:34 -0800

    700

--- app/Services/Resources/Zenai.php
+++ app/Services/Resources/Zenai.php
@@ -7,59 +7,80 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
-use voku\helper\HtmlDomParser;
 
 final class Zenai {
 	public function __invoke($keyword) {
 
-        $date = date('ymd');
+    if($keyword == 700){
+        $content = "=====节目分组1=====\n【701】灵程真言\n【702】喜乐灵程\n【703】认识你真好 \n【704】真爱驻我家\n【705】尔道自建\n=====节目分组2=====\n【698】馒头的对话\n【619】拥抱每一天\n【610】星动一刻\n【621】真道分解\n【620】旷野吗哪";
+        return [
+            'type' => 'text',
+            'data' => ['content' => $content]
+        ];
+    }
+
         $res = [
-            '喜乐灵程' =>[ // 1-7
-                'imageCode' => 'tljd',
-                'code' => "tljd",
-                'textCode' => "tljd",
-            ],
-            '灵程真言' =>[ // 1 - 7
-                'imageCode' => 'tllczy',
-                'image' => 'https://img-1253798207.file.myqcloud.com/tllczy.png',
+            // https://d3pc7cwodb2h3p.cloudfront.net/all_vof_songs.json
+            '701' =>[ // 1 - 7
+                'title' => '灵程真言',
                 'code' => "tllczy",
-                'textCode' => "tllczy",
             ],
-            '真爱驻我家' =>[ // 1 - 5 真爱驻我家 是周日周一休息
-                'imageCode' => 'tltl',
-                'code' => "tlpc",
-                'textCode' => "tlpc",
+            '702' =>[ // 1-7
+                'title' => '喜乐灵程',
+                'code' => "tljd",
             ],
-            '认识你真好' =>[
-                'imageCode' => 'vof',
+            '703' =>[//1-7
+                'title' => '认识你真好',
                 'code' => "vof",
-                'textCode' => "vof",
+            ],
+            '704' =>[ // 1 - 5 真爱驻我家 是周一休息
+                'title' => '真爱驻我家',
+                'code' => "tltl",
+            ],
+            '705' =>[
+                'title' => '尔道自建',
+                'code' => "edzj",
             ],
         ];
 
         if(in_array($keyword, array_keys($res))){
-            $res = $res[$keyword];
-            $url = "http://dailyaudio-1253798207.file.myqcloud.com/{$res['code']}{$date}.mp3";
-            if($keyword == '认识你真好')
-                $url = "https://febc-1253798207.file.myqcloud.com/vof/{$res['code']}{$date}.mp3";
-            if($keyword == '真爱驻我家' && (date('N')==1 || date('N')==7)) return null;
-            return [
-            	'type' => 'music',
-            	"data"=> [
-                    "url" => $url,
-                    'title' => "【{$keyword}】{$date}",
-                    'description' => '真爱驻我家',
-                ],
-                'addition' =>[
-                    'type' => 'link',
-                    'data' => [
-                        'image' => "https://img-1253798207.file.myqcloud.com/{$res['imageCode']}.png",
-                        "url" => "https://d7jf0n9s4n8dc.cloudfront.net/html/{$res['textCode']}/{$res['textCode']}{$date}.html",
-                        'title' => "【{$keyword}】{$date}",
-                        'description' => '节目文本 真爱驻我家',
+            $cacheKey = "xbot.700.{$keyword}";
+            $data = Cache::get($cacheKey, false);
+            if($data) return $data;
+            
+            if(!$data){
+                $res = $res[$keyword];
+                $response = Http::get("https://d3pc7cwodb2h3p.cloudfront.net/all_{$res['code']}_songs.json");
+                $json =$response->json();
+                $jdata = last($json);
+                $title = "【{$keyword}】{$res['title']}-".substr($jdata['time'],2);
+                $image = "https://d33tzbj8j46khy.cloudfront.net/{$res['code']}.png";
+                $data = [
+                    'type' => 'music',
+                    "data"=> [
+                        "url" => $jdata['path'],
+                        'title' => $title,
+                        'description' => $jdata['title'],
+                        'image' => $image,
                     ],
-                ],
-            ];
+                ];
+                if($jdata['hasArtistHtml']){
+                    $addition = [
+                        'type' => 'link',
+                        'data' => [
+                            'image' => $image,
+                            "url" => "https://d7jf0n9s4n8dc.cloudfront.net/html/{$res['code']}/{$res['code']}".substr($jdata['time'],2).".html",
+                            'title' => $title,
+                            'description' => '节目文本-'. $jdata['title'],
+                        ],
+                    ];
+                    $data = array_merge($data,['addition'=>$addition]);
+                    Cache::put($cacheKey, $data, strtotime('tomorrow') - time());
+                    return $data;
+                }
+                Cache::put($cacheKey, $data, strtotime('tomorrow') - time());
+                return $data
+            }
         }
         return null;
 	}
